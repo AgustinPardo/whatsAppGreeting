ae2700f603d1a5e6f61744bf97238ff772c2c238
AgustinPardo <agu_pardo@hotmail.com>
2021-01-22 00:26:00 -0300

    Erro handling

--- pruebas_class.py
+++ pruebas_class.py
@@ -4,47 +4,67 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.keys import Keys
 
+from selenium.common.exceptions import InvalidArgumentException
+
+import sys
+
 class Driver:
 
-    def __init__(self, browser_user="prueba2"):
+    def __init__(self, greet_string, browser_user="prueba2"):
+
         self.browser_user=browser_user
+        self.greet_string=greet_string
 
         self.driver=self.connect()
 
     def connect(self):
-        # ChromeOptions allows us use the userdata of chrome 
-        # so that you don't have to sign in manually everytime. 
-        chropt = webdriver.ChromeOptions()
-        # adding userdata argument to ChromeOptions object 
-        chropt.add_argument("user-data-dir=/home/agustin/.config/google-chrome/"+self.browser_user)        
-        # Creating a Chrome webdriver object 
-        driver = webdriver.Chrome(options = chropt)
-        driver.get("https://web.whatsapp.com/")
-        # delay added to give time for all elements to load 
-        time.sleep(10)
+        try:
+            # ChromeOptions allows us use the userdata of chrome 
+            # so that you don't have to sign in manually everytime. 
+            chropt = webdriver.ChromeOptions()
+            # adding userdata argument to ChromeOptions object 
+            chropt.add_argument("user-data-dir=/home/agustin/.config/google-chrome/"+self.browser_user) 
+            # Creating a Chrome webdriver object 
+            driver = webdriver.Chrome(options = chropt)
+            driver.get("https://web.whatsapp.com/")
+            # delay added to give time for all elements to load 
+            time.sleep(10)
+
+        except InvalidArgumentException as exception:
+            print("Error:")
+            print(exception)
+            print ("Close all the Browser windows cotrolled by Selenium and re-start the execution!")
+            sys.exit(1)
+
 
         return driver
 
     def getUser(self):
-        # Hacer
-        self.driver.find_elements_by_class_name("rlUm6")[0].click()
-        time.sleep(1)
-        user_name= self.driver.find_elements_by_class_name("_1awRl")[0].text
-        self.driver.find_elements_by_class_name("hYtwT")[0].click()
+        try:
+            self.driver.find_elements_by_class_name("rlUm6")[0].click()
+            time.sleep(1)
+            user_name= self.driver.find_elements_by_class_name("_1awRl")[0].text
+            self.driver.find_elements_by_class_name("hYtwT")[0].click()
+        except IndexError as exception:
+            print("Error:")
+            print ("Log-in to WhatsApp web scanning the logo!")
+            sys.exit(1)
 
-        return user_name
+            return user_name
         
     def closeDriver(self):
 
         self.driver.close()
+        
         return 0
 
 class Panel:
 
-    def __init__(self, driver, user, chats=[], timeout=time.time()+60):
+    def __init__(self, driver, user, greet_string, chats=[], timeout=time.time()+60):
 
         self.driver=driver
         self.user=user
+        self.greet_string=greet_string
         self.chats=chats
         self.timeout=timeout
 
@@ -58,14 +78,12 @@ class Panel:
             for driver_chat in driver_chats:
 
                 driver_chat.click()
-                chat=Chat(self.driver, self.user)
+                chat=Chat(self.driver, self.user , self.greet_string)
 
                 if chat.greet() and (chat.name() not in self.chats):
 
-                    greet='OK'
-                    
                     input_box=self.driver.find_element_by_class_name('DuUXI')
-                    input_box.send_keys(greet+Keys.ENTER)
+                    input_box.send_keys(self.greet_string+Keys.ENTER)
 
                 self.chats.append(chat.name())
 
@@ -80,10 +98,11 @@ class Panel:
 
 class Chat:
 
-    def __init__(self, driver, user_name):
+    def __init__(self, driver, user_name, greet_string):
 
         self.driver=driver
         self.user_name=user_name
+        self.greet_string=greet_string
 
     def name(self):
         return self.driver.find_elements_by_class_name("YEe1t")[0].text
@@ -107,28 +126,33 @@ class Chat:
 
         return chat_msgs
 
+    def parseDate():
+        pass
+
     def greet(self):
 
         chat_msgs = self.readMsgs()
         word=["pepito125"]
+
         res=False
-        final=False
+        user_greet=True
+        else_greet=False
 
         for msg in chat_msgs:
+            print(msg)
             for element in word:
-                if element in msg[2].lower():
-                    res=True
-                else:
-                    res=False
-            if res and msg[0] != self.user_name:
-                final= True
+                if element in msg[2].lower() and msg[0] != self.user_name :
+                    else_greet=True
 
-        return final
+            if msg[2] == self.greet_string and msg[0] == self.user_name:
+                user_greet=False
+
+        return else_greet and user_greet
 
 if __name__ == '__main__':
 
 
-    driver = Driver()
-    panel=Panel(driver.driver, driver.getUser())
+    driver = Driver("Bien02")
+    panel=Panel(driver.driver, driver.getUser(), driver.greet_string)
     panel.greetChats()
     driver.closeDriver()
